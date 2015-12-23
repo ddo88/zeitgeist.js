b8461241bdd20e531c3290acf55f92504f170e29
Daniel D <ddo88@hotmail.com>
2015-12-22 20:29:09 -0500

    append get params method

--- zeitgeist.js
+++ zeitgeist.js
@@ -32,6 +32,22 @@
             }, 0);
             return dfd.promise();
         }
+//retrieve url params in object
+    Zg.GetParams= function() {
+            var params = {};
+            if (location.search) {
+                var parts = location.search.substring(1).split('&');
+
+                for (var i = 0; i < parts.length; i++) {
+                    var nv = parts[i].split('=');
+                    if (!nv[0]) continue;
+                    params[nv[0]] = nv[1] || true;
+                }
+            }
+            return params;
+
+        }    
+        
         
         
         
